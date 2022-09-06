d79b069543b236d3efd0835406f3521a721adba4
Bailey Cage <bailey.cage@gmail.com>
2022-09-06 17:29:28 -0400

    deliverable 3 - toggle good dog

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,25 @@ function App() {
     setDisplayDog(dog);
   }
 
+  function toggleGoodDog () {
+    const updateDog = {
+      ...displayDog,
+      isGoodDog: !displayDog.isGoodDog
+    }
+    setDisplayDog(updateDog)
+    fetch(`http://localhost:3001/pups/${displayDog.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(updateDog)
+    })
+  }
+
+  function toggleGoodDogFilter () {
+    console.log('hi');
+  }
+
   const dogBarSpans = pupsAry.map(dog => {
     return <span key={dog.id} onClick={() => displayDogDetails(dog)}>{dog.name}</span>
   });
@@ -21,13 +40,13 @@ function App() {
   const dogDetails = (<>
     <img src={displayDog.image} alt={displayDog.name} />
     <h2>{displayDog.name}</h2>
-    <button>{displayDog.isGoodDog ? "Good Dog!" : "Bad Dog!"}</button>
+    <button onClick={toggleGoodDog}>{displayDog.isGoodDog ? "Good Dog!" : "Bad Dog!"}</button>
   </>);
 
   return (
     <div className="App">
       <div id="filter-div">
-        <button id="good-dog-filter">Filter good dogs: OFF</button>
+        <button id="good-dog-filter" onClick={toggleGoodDogFilter}>Filter good dogs: OFF</button>
       </div>
       <div id="dog-bar">{dogBarSpans}</div>
       <div id="dog-summary-container">
