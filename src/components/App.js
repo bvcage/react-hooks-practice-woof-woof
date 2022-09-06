d5a4c8d4ac2010db21fbe64ec0412163d1cb017f
Bailey Cage <bailey.cage@gmail.com>
2022-09-06 17:02:47 -0400

    deliverable 1 - show pups in dog bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [pupsAry, setPupsAry] = useState([])
+
+  const pupsData = useEffect(() => {
+    fetch('http://localhost:3001/pups')
+    .then(r => r.json())
+    .then(data => setPupsAry(data));
+  }, []);
+
   return (
     <div className="App">
       <div id="filter-div">
         <button id="good-dog-filter">Filter good dogs: OFF</button>
       </div>
-      <div id="dog-bar"></div>
+      <div id="dog-bar">{
+        pupsAry.map(dog => {
+          return <span>{dog.name}</span>
+        })
+      }</div>
       <div id="dog-summary-container">
         <h1>DOGGO:</h1>
         <div id="dog-info"></div>
