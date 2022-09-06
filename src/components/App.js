467002042a95c6409c215624d0720b68f1c1499c
Bailey Cage <bailey.cage@gmail.com>
2022-09-06 17:21:46 -0400

    deliverable 2 - show more info about each pup

--- src/components/App.js
+++ src/components/App.js
@@ -2,26 +2,37 @@ import React, { useEffect, useState } from "react";
 
 function App() {
   const [pupsAry, setPupsAry] = useState([])
+  const [displayDog, setDisplayDog] = useState({});
 
-  const pupsData = useEffect(() => {
+  useEffect(() => {
     fetch('http://localhost:3001/pups')
     .then(r => r.json())
     .then(data => setPupsAry(data));
   }, []);
+
+  function displayDogDetails (dog) {
+    setDisplayDog(dog);
+  }
+
+  const dogBarSpans = pupsAry.map(dog => {
+    return <span key={dog.id} onClick={() => displayDogDetails(dog)}>{dog.name}</span>
+  });
+
+  const dogDetails = (<>
+    <img src={displayDog.image} alt={displayDog.name} />
+    <h2>{displayDog.name}</h2>
+    <button>{displayDog.isGoodDog ? "Good Dog!" : "Bad Dog!"}</button>
+  </>);
 
   return (
     <div className="App">
       <div id="filter-div">
         <button id="good-dog-filter">Filter good dogs: OFF</button>
       </div>
-      <div id="dog-bar">{
-        pupsAry.map(dog => {
-          return <span>{dog.name}</span>
-        })
-      }</div>
+      <div id="dog-bar">{dogBarSpans}</div>
       <div id="dog-summary-container">
         <h1>DOGGO:</h1>
-        <div id="dog-info"></div>
+        <div id="dog-info">{dogDetails}</div>
       </div>
     </div>
   );
