6a396570b4815fe31400269ad39f0b778212f7e2
Bailey Cage <bailey.cage@gmail.com>
2022-09-06 21:09:00 -0400

    deliverable 4 - filter good dogs

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,44 @@
 import React, { useEffect, useState } from "react";
 
 function App() {
-  const [pupsAry, setPupsAry] = useState([])
-  const [displayDog, setDisplayDog] = useState({});
+  const [allDogsAry, setAllDogsAry] = useState([]);
+  const [dogBarAry, setDogBarAry] = useState([]);
+  const [dogToDisplay, setDogToDisplay] = useState({});
+  const [goodDogsOnly, setGoodDogsOnly] = useState(false);
 
   useEffect(() => {
     fetch('http://localhost:3001/pups')
     .then(r => r.json())
-    .then(data => setPupsAry(data));
+    .then(data => {
+      setAllDogsAry(data);
+      setDogBarAry(data);
+    });
   }, []);
 
   function displayDogDetails (dog) {
-    setDisplayDog(dog);
+    setDogToDisplay(dog);
   }
 
-  function toggleGoodDog () {
+  function toggleDogIsGood () {
     const updateDog = {
-      ...displayDog,
-      isGoodDog: !displayDog.isGoodDog
+      ...dogToDisplay,
+      isGoodDog: !dogToDisplay.isGoodDog
     }
-    setDisplayDog(updateDog)
-    fetch(`http://localhost:3001/pups/${displayDog.id}`, {
+    setDogToDisplay(updateDog);
+    
+    const updatedDogsAry = allDogsAry.map(dog => {
+      if (dog.id === updateDog.id) return updateDog;
+      else return dog;
+    })
+    setAllDogsAry(updatedDogsAry);
+    if (goodDogsOnly) {
+      const goodDogAry = updatedDogsAry.filter(dog => dog.isGoodDog)
+      updateDogBarAry(goodDogAry);
+    } else {
+      updateDogBarAry(updatedDogsAry);
+    }
+    
+    fetch(`http://localhost:3001/pups/${dogToDisplay.id}`, {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json"
@@ -30,23 +48,35 @@ function App() {
   }
 
   function toggleGoodDogFilter () {
-    console.log('hi');
+    const newStatus = !goodDogsOnly;
+    setGoodDogsOnly(newStatus);
+
+    if (newStatus) {
+      const goodDogAry = allDogsAry.filter(dog => dog.isGoodDog);
+      updateDogBarAry(goodDogAry);
+    } else {
+      updateDogBarAry(allDogsAry);
+    }
   }
 
-  const dogBarSpans = pupsAry.map(dog => {
+  function updateDogBarAry (newAry) {
+    setDogBarAry(newAry);
+  }
+
+  let dogBarSpans = dogBarAry.map(dog => {
     return <span key={dog.id} onClick={() => displayDogDetails(dog)}>{dog.name}</span>
   });
 
   const dogDetails = (<>
-    <img src={displayDog.image} alt={displayDog.name} />
-    <h2>{displayDog.name}</h2>
-    <button onClick={toggleGoodDog}>{displayDog.isGoodDog ? "Good Dog!" : "Bad Dog!"}</button>
+    <img src={dogToDisplay.image} alt={dogToDisplay.name} />
+    <h2>{dogToDisplay.name}</h2>
+    <button onClick={toggleDogIsGood}>{dogToDisplay.isGoodDog ? "Good Dog!" : "Bad Dog!"}</button>
   </>);
 
   return (
     <div className="App">
       <div id="filter-div">
-        <button id="good-dog-filter" onClick={toggleGoodDogFilter}>Filter good dogs: OFF</button>
+        <button id="good-dog-filter" onClick={toggleGoodDogFilter}>Filter good dogs: {goodDogsOnly? "ON" : "OFF"}</button>
       </div>
       <div id="dog-bar">{dogBarSpans}</div>
       <div id="dog-summary-container">
